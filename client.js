de658545ac8fee940380bac5d4c1cdff0e21c47f
Tina Tram <tinatram07@gmail.com>
2024-04-27 17:36:14 -0500

    basic page navigation

--- client.js
+++ client.js
@@ -1,1 +1,9 @@
 console.log('This file works!');
+
+function routeNewPage() {
+     window.location = 'restaurants.html';
+    }
+
+function routeFoodPage() {
+    window.location = 'food-list.html';
+}
